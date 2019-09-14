cd78dfdbe57c0f198648dc8a79c30acbbc3f5db6
vshashank7 <vshashank7@outlook.com>
2019-09-15 04:27:04 +0530

    models update

--- complain/admin.py
+++ complain/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student , complaintPost
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(complaintPost)
--- grievanceportal/urls.py
+++ grievanceportal/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from . import settings
 from django.urls import include, path
 from complain import urls
 from deptadmin import urls
 from assignee import urls
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,5 @@ urlpatterns = [
     path('deptadmin/', include(urls)),
     path('assignee/',include(urls)),
 ]
+urlpatterns+=staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
